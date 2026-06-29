017157ca54733dde94ec5ca5901f22edf6a90eb5
Hershy1357 <hershy1357@gmail.com>
2026-06-29 11:00:03 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,17 @@
-const CACHE = 'member-lookup-v4';
-const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
-
+// Force clear all caches
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(FILES);
-    })
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
-      return Promise.all(keys.map(function(k) { return caches.delete(k); }))
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
     })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
